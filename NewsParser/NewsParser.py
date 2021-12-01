ba087f20dd9949c9225fd3a474aef03e5840dc5d
Tapo4ek-exe <am14102001@gmail.com>
2021-12-02 00:27:31 +0300

    Изменен тип данных хранимых дат (datetime вместо str). Парсинг страниц осуществляется с последней страницы к первой (от старых новостей к новым). Реализовано хранение и обновление данных в MongoDB. Добавлены комментарии к коду.

--- NewsParser/NewsParser.py
+++ NewsParser/NewsParser.py
@@ -1,14 +1,21 @@
+import datetime
+
 from bs4 import BeautifulSoup
 import requests
 import time
 import csv
+import pymongo
 
+
+MONGO = "mongodb+srv://admin:admin@cluster0.bwhk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 URL = "https://v1.ru/text/"
 HEADERS = {
     "Accept": "text / html, application / xhtml + xml, application / xml; q = 0.9, image / avif, image / webp, * / *;q = 0.8",
     "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv: 94.0) Gecko / 20100101 Firefox / 94.0"
 }
 PATH = "news.csv"
+ID = 1
+
 
 # Парсинг нвостей с сайта V1.RU
 def main():
@@ -20,42 +27,59 @@ def main():
 
     # Парсим все страницы
     news = []
-    pagesCount = 10
     startTime = time.time()
-    for pageNumber in range(1, pagesCount + 1):
-        print(f"[INFO] Парсинг страницы {pageNumber} из {pagesCount}")
+    for pageNumber in range(pagesCount, 0, -1):
+        print(f"[INFO] Парсинг страницы {pagesCount + 1 - pageNumber} из {pagesCount}")
         response = requests.get(url=f"{URL}?page={pageNumber}", headers=HEADERS)
         parsePage(news, response.text)
     endTime = time.time()
     print(f"[INFO] Прошло времени: {endTime - startTime} сек")
-    saveCSV(news, PATH)
-    print(news[0]["Content"])
+
+    # Сохраняем данные в базу данных (обновляем старые)
+    print(f"[INFO] Сохранение в БД...")
+    client = pymongo.MongoClient(MONGO)
+    db = client.V1News
+    coll = db.news
+    docs_count = coll.count_documents({})
+    for id in range(1, len(news) + 1):
+        if id > docs_count:
+            coll.insert_one(news[id - 1])
+        else:
+            current = {"Id" : id}
+            new = {"$set": {"Views": news[id - 1]["Views"],
+                            "Comments": news[id - 1]["Comments"]}}
+            coll.update_one(current, new)
+    print("[INFO] Успешно!")
 
 
+# Парсинг страницы с новостями V1.RU
 def parsePage(news: list, html):
     HOST = "https://v1.ru"
     soup = BeautifulSoup(html, "html.parser")
     items = soup.findAll("article", "IJamp")
-    for item in items:
-        heading = None; date = None; link = None; views = None; comments = None;
-        heading = item.find("h2", "IJm9").getText(strip=True)
-        if item.find("div", "IJal3") is None:
-            continue
-        else:
+    for item in reversed(items):
+        heading = None; date = None; link = None; views = None; comments = None;    # очищаем переменные
+        heading = item.find("h2", "IJm9").getText(strip=True)                       # получаем название новости
+        if item.find("div", "IJal3") is None:                                       # eсли нет краткого описания,
+            continue                                                                #   значит, это просто реклама - пропускаем ее,
+        else:                                                                       # иначе получаем краткое описание новости
             description = item.find("div", "IJal3").getText(strip=True)
-        date = item.find("div", "IJain IJam-").getText(strip=True)
-        link = item.find("h2", "IJm9").find("a").get("href")
-        if link.find("v1.ru") == -1:
-            link = HOST + link
-        content = parseNewsContent(link)
-        stats = item.findAll("span", "H3gt")
+        date = datetime.datetime\
+            .fromisoformat(item.find("div", "IJain IJam-")                          # получаем дату
+                                .find("time").get("datetime"))
+        link = item.find("h2", "IJm9").find("a").get("href")                        # получаем ссылку на статью
+        if link.find("v1.ru") == -1:                                                # если ссылка не содержит в себе хоста,
+            link = HOST + link                                                      #   то добавляем его
+        content = parseNewsContent(link)                                            # парсим содержимое статьи
+        stats = item.findAll("span", "H3gt")                                        # получаем данные о просмотрах и комментариях
         views = stats[0].getText(strip=True)
-        if len(stats) < 2 or stats[1].getText(strip=True).find("Обсудить") != -1:
-            comments = ""
-        else:
+        if len(stats) < 2 or stats[1].getText(strip=True).find("Обсудить") != -1:   # если комментариев еще нет,
+            comments = ""                                                           #   записываем пустую строку
+        else:                                                                       # иначе парсим кол-во комментариев
             comments = stats[1].getText(strip=True)
-
-        news.append({
+        global ID
+        news.append({                                                               # добавляем все данные о статье в список
+            "_id": ID,
             "Heading": heading,
             "Description": description,
             "Date": date,
@@ -64,25 +88,31 @@ def parsePage(news: list, html):
             "Views": views,
             "Comments": comments
         })
+        ID += 1
 
+
+# Парсинг содержимого статьи
 def parseNewsContent(URL):
     response = requests.get(url=URL, headers=HEADERS)
     soup = BeautifulSoup(response.text, "html.parser")
     try:
-        contentList = soup.find("div", "I5pv").findAll("div", "INant")
+        contentList = soup.find("div", "I5pv").findAll("div", "INant")  # находим все абзацы статьи
     except:
         contentList = []
     result = ""
-    for content in contentList:
+    for content in contentList:                                         # записываем все абзацы в одну строку
         result += content.getText(strip=True, separator=" ") + "\n"
     return result
 
+
+# Сохранение новостей в файл CSV
 def saveCSV(newsList: list, path: str):
     with open(path, "w", newline="", encoding="cp1251", errors="ignore") as file:
         writer = csv.writer(file, delimiter=";")
         writer.writerow(["Заголовок", "Описание", "Дата", "Ссылка", "Содержание", "Количество просмотров", "Количество комментариев"])
         for news in newsList:
             writer.writerow([news["Heading"], news["Description"], news["Date"], news["Link"], news["Content"], news["Views"], news["Comments"]])
+
 
 if __name__ == '__main__':
     main()
--- NewsParser/NewsParserAsync.py
+++ NewsParser/NewsParserAsync.py
@@ -1,22 +1,27 @@
+import datetime
+import operator
+
 from bs4 import BeautifulSoup
-import requests
 import time
 import csv
 import asyncio
 import aiohttp
+import pymongo
 
+MONGO = "mongodb+srv://admin:admin@cluster0.bwhk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 URL = "https://v1.ru/text/"
 HEADERS = {
     "Accept": "text / html, application / xhtml + xml, application / xml; q = 0.9, image / avif, image / webp, * / *;q = 0.8",
     "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv: 94.0) Gecko / 20100101 Firefox / 94.0"
 }
 PATH = "news_async.csv"
+ID = 1
 
 async def getPageData(session, page, pagesCount, news):
     async with session.get(url=f"{URL}?page={page}", headers=HEADERS) as response:
         responseText = await response.text()
         await parsePage(session, news, responseText)
-        print(f"[INFO] Парсинг страницы {page} из {pagesCount}")
+        print(f"[INFO] Парсинг страницы {pagesCount + 1 - page} из {pagesCount}")
 
 
 async def gatherData(news):
@@ -30,8 +35,7 @@ async def gatherData(news):
 
         # Парсим все страницы
         tasks = []
-        pagesCount = 10
-        for page in range(1, pagesCount + 1):
+        for page in range(pagesCount, 0, -1):
             task = asyncio.create_task(getPageData(session, page, pagesCount, news))
             tasks.append(task)
 
@@ -46,36 +50,59 @@ def main():
         asyncio.get_event_loop().run_until_complete(gatherData(news))
         endTime = time.time()
         print(f"[INFO] Прошло времени: {endTime - startTime} сек")
-        saveCSV(news, PATH)
-    except Exception as ex:
-        print(ex)
 
+        # Сохраняем данные в базу данных (обновляем старые)
+        print(f"[INFO] Сохранение в БД...")
+        client = pymongo.MongoClient(MONGO)
+        db = client.V1News
+        coll = db.news
+        docs_count = coll.count_documents({})
+
+        ## Сортируем список по датам
+        news.sort(key=operator.itemgetter("Date"))
+        for i in range(0, len(news)):
+            news[i]["_id"] = i + 1
+
+        for id in range(1, len(news) + 1):
+            if id > docs_count:
+                coll.insert_one(news[id - 1])
+            else:
+                current = {"Id": id}
+                new = {"$set": {"Views": news[id - 1]["Views"],
+                                "Comments": news[id - 1]["Comments"]}}
+                coll.update_one(current, new)
+        print("[INFO] Успешно!")
+    except Exception as ex:
+        print(f"[ERROR] {ex}")
 
 
 async def parsePage(session, news: list, html):
     HOST = "https://v1.ru"
     soup = BeautifulSoup(html, "html.parser")
     items = soup.findAll("article", "IJamp")
-    for item in items:
-        heading = None; date = None; link = None; views = None; comments = None;
-        heading = item.find("h2", "IJm9").getText(strip=True)
-        if item.find("div", "IJal3") is None:
-            continue
-        else:
+    for item in reversed(items):
+        heading = None; date = None; link = None; views = None; comments = None;    # очищаем переменные
+        heading = item.find("h2", "IJm9").getText(strip=True)                       # получаем название новости
+        if item.find("div", "IJal3") is None:                                       # eсли нет краткого описания,
+            continue                                                                #   значит, это просто реклама - пропускаем ее,
+        else:                                                                       # иначе получаем краткое описание новости
             description = item.find("div", "IJal3").getText(strip=True)
-        date = item.find("div", "IJain IJam-").getText(strip=True)
-        link = item.find("h2", "IJm9").find("a").get("href")
-        if link.find("v1.ru") == -1:
-            link = HOST + link
-        content = await parseNewsContent(session, link)
-        stats = item.findAll("span", "H3gt")
+        date = datetime.datetime\
+            .fromisoformat(item.find("div", "IJain IJam-")                          # получаем дату
+                                .find("time").get("datetime"))
+        link = item.find("h2", "IJm9").find("a").get("href")                        # получаем ссылку на статью
+        if link.find("v1.ru") == -1:                                                # если ссылка не содержит в себе хоста,
+            link = HOST + link                                                      #   то добавляем его
+        content = await parseNewsContent(session, link)                                   # парсим содержимое статьи
+        stats = item.findAll("span", "H3gt")                                        # получаем данные о просмотрах и комментариях
         views = stats[0].getText(strip=True)
-        if len(stats) < 2 or stats[1].getText(strip=True).find("Обсудить") != -1:
-            comments = ""
-        else:
+        if len(stats) < 2 or stats[1].getText(strip=True).find("Обсудить") != -1:   # если комментариев еще нет,
+            comments = ""                                                           #   записываем пустую строку
+        else:                                                                       # иначе парсим кол-во комментариев
             comments = stats[1].getText(strip=True)
-
-        news.append({
+        global ID
+        news.append({                                                               # добавляем все данные о статье в список
+            "_id": ID,
             "Heading": heading,
             "Description": description,
             "Date": date,
@@ -84,22 +111,25 @@ async def parsePage(session, news: list, html):
             "Views": views,
             "Comments": comments
         })
+        ID += 1
 
 
+# Парсинг содержимого статьи
 async def parseNewsContent(session, URL):
     async with session.get(url=URL, headers=HEADERS) as response:
         responseText = await response.text()
         soup = BeautifulSoup(responseText, "html.parser")
         try:
-            contentList = soup.find("div", "I5pv").findAll("div", "INant")
+            contentList = soup.find("div", "I5pv").findAll("div", "INant")  # находим все абзацы статьи
         except:
             contentList = []
         result = ""
-        for content in contentList:
+        for content in contentList:                                         # записываем все абзацы в одну строку
             result += content.getText(strip=True, separator=" ") + "\n"
         return result
 
 
+# Сохранение новостей в файл CSV
 def saveCSV(newsList: list, path: str):
     with open(path, "w", newline="", encoding="cp1251", errors="ignore") as file:
         writer = csv.writer(file, delimiter=";")
